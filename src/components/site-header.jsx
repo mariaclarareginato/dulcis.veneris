b8390b16ff005d312b3029cbbfc1f28f2fcadaea
MARIA CLARA <24250876@educ123.sp.senai.br>
2025-09-18 16:29:17 -0300

    definição paleta de cores e logo principal - Maria Clara Reginato 16:29  18/09/25

--- src/components/site-header.jsx
+++ src/components/site-header.jsx
@@ -29,8 +29,12 @@ export function SiteHeader() {
     <header className="flex h-[--header-height] shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-[--header-height]">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
+
         <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4" />
-        <h1 className="text-base font-medium text-center w-full">Dulcis Veneris</h1>
+      <h1 className="text-[40px] text-red-800 font-bold text-center w-full">
+  ✧ Dulcis Veneris ✧
+</h1>
+
         <div className="ml-auto flex items-center gap-2">
          
           <DropdownMenu>
