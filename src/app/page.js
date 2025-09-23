eda55f6be2e9b8f01e1e54ab2f87f9fd716cb552
MARIA CLARA <24250876@educ123.sp.senai.br>
2025-09-23 14:44:29 -0300

    Página 404 e home estilizadas

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,55 @@
+"use client";
+
+// Importações
+
+import React from "react";
+import { useRouter } from 'next/navigation';
+ 
 
 
 export default function Home() {
+    const router = useRouter();
+    
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-bold text-center w-full">Dulcis Veneris</h1>
-        </main>
+    <div className="relative w-full h-screen overflow-hidden">
+    
+
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-6">
+  <h1 className="text-2xl md:text-3xl font-bold text-gray-200 leading-snug max-w-2xl">
+    Bem-Vindo ao sistema Dulcis Veneris! 
+  </h1>
+
+  {/* Logo */}
+
+  <img
+  
+  src="logos/logo.png"
+   
+  />
+
+ 
+      {/* Botões */}
+ 
+        <br />
+ 
+        <div className="flex gap-8 ">
+          <button
+            className="inline-flex items-center justify-center
+                       text-base font-semibold text-white
+                       bg-red-800 rounded-2xl
+                       !px-8 !py-4    
+                       shadow-lg
+                       transition-all duration-300 ease-out
+                       hover:bg-red-600 hover:scale-105 hover:shadow-xl
+                       active:scale-95 focus:outline-none focus:ring-4 focus:ring-red-500/40"onClick={() => router.push("/login")}
+          >
+            Entrar
+          </button>
+ 
+        
+        </div>
+      </div>
     </div>
   );
 }
+ 
