e0f29be5d4ca0996c95fc5b4ec8b8a3549736b0f
MARIA CLARA <24250876@educ123.sp.senai.br>
2025-10-16 16:37:23 -0300

    Estiização página login

--- src/app/carrinho/layout.jsx
+++ src/app/carrinho/layout.jsx
@@ -1,5 +1,6 @@
 
 import { AppSidebar } from "@/components/app-sidebar"
+import { SiteFooter } from "@/components/site-footer"
 import { SiteHeader } from "@/components/site-header"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 
@@ -16,6 +17,7 @@ export default function CaixaLayout({ children }) {
         <SiteHeader />
         <div className="flex flex-1 flex-col p-6">
           {children}
+          <SiteFooter />
         </div>
       </SidebarInset>
     </SidebarProvider>
