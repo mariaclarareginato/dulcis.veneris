932eeb4556aa17ed14b7dad8a8bb7bd76cdd4857
MARIA CLARA <24250876@educ123.sp.senai.br>
2025-11-14 16:54:49 -0300

    Páginas de gerente finalizadas e começo das página de adm (matriz)

--- src/app/matriz/layout.jsx
+++ src/app/matriz/layout.jsx
@@ -1,7 +1,7 @@
 
-import { AppSidebar } from "@/components/app-sidebar2";
+import { AppSidebar2 } from "@/components/app-sidebar2";
 import { SiteFooter } from "@/components/site-footer";
-import { SiteHeader } from "@/components/site-header3";
+import { SiteHeader3 } from "@/components/site-header3";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 export default function MatrizLayout({ children }) {
@@ -12,9 +12,9 @@ export default function MatrizLayout({ children }) {
         "--header-height": "calc(var(--spacing) * 12)",
       }}
     >
-      <AppSidebar variant="inset" />
+      <AppSidebar2 variant="inset" />
       <SidebarInset>
-        <SiteHeader />
+        <SiteHeader3 />
         <div className="flex flex-1 flex-col p-6">
           {children}
           <SiteFooter />
